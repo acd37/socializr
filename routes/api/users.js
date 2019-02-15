e827103acdd1ed22eeda5b7b07cb7fd514af420d
Alec Down <alecdown@gmail.com>
2019-02-14 19:14:55 -0700

    initial commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load user model
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests the users route
@@ -7,6 +12,73 @@ const router = express.Router();
 router.get('/test', (req, res) => {
   res.json({
     msg: 'Users routes works'
+  });
+});
+
+// @route   POST api/users/register
+// @desc    Register the user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: 'Email already exists.'
+      });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // size of gravatar image
+        r: 'pg', // image rating
+        d: 'mm' // default image
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route   POST api/users/login
+// @desc    Login the user // return the JWT
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check the user exists
+    if (!user) {
+      return res.status(404).json({ email: 'User not found.' });
+    }
+
+    // Check the password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // generate the token
+        res.json({ msg: 'Success' });
+      } else {
+        return res
+          .status(400)
+          .json({ password: 'User password could not be validated.' });
+      }
+    });
   });
 });
 
