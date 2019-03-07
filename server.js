cda1d0777b8fcbd347cbd7213f02ffd9aac9a57a
Alec Down <alecdown@gmail.com>
2019-03-07 10:40:30 -0600

    fixed spelling error in server

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/api/posts', posts);
 // server static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(expres.static('client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
