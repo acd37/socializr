80944620e5db975e67c902425b8da35e67bd797a
Alec Down <alecdown@gmail.com>
2019-03-08 10:56:57 -0600

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://acd37:password1@ds237574.mlab.com:37574/socializr',
-    secretOrKey: 'YXNkZjc4MHFuYjkwM25mZHMsZjsnS0QyXTQwMjM4SDRCU0RBRlNORE0zMTk=',
-    googleBooksKey: 'AIzaSyAKJiqFViOGV0ApS1cPVFOIbayqQw42bPg'
-};
